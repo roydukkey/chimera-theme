843f42f9e244bd2ad67c178e9c8ff4a07546100b
roydukkey <contact@changelog.me>
2020-09-01 16:07:43 -0400

    Fix order of colors

--- src/colors.js
+++ src/colors.js
@@ -30,6 +30,10 @@ colors.ForegroundDark1 = colors.Foreground.darken(0.35);
 colors.ForegroundDark2 = colors.Foreground.darken(0.5);
 colors.ForegroundPurpleMix = colors.Foreground.mix(colors.Purple);
 
+colors.GreenDark1 = colors.Green.darken(0.35).desaturate(0.65);
+
+colors.GreenYellowMix = colors.Green.mix(colors.Yellow);
+
 colors.OrangeDark1 = colors.Orange.darken(0.50).desaturate(0.65);
 
 colors.PinkDark1 = colors.Pink.darken(0.30).desaturate(0.50);
@@ -38,10 +42,6 @@ colors.PinkDark3 = colors.Pink.darken(0.50).desaturate(0.70);
 
 colors.PurpleDark1 = colors.Purple.darken(0.25).desaturate(0.55);
 colors.PurpleDark2 = colors.Purple.darken(0.50).desaturate(0.75);
-
-colors.GreenDark1 = colors.Green.darken(0.35).desaturate(0.65);
-
-colors.GreenYellowMix = colors.Green.mix(colors.Yellow);
 
 colors.RedDark1 = colors.Red.darken(0.35).desaturate(0.65);
 
