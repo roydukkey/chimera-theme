5e74345b920b8ea1ee77d082f3a44cee64473c82
roydukkey <contact@changelog.me>
2020-10-01 11:58:59 -0400

    Add main entry for extension

---
+++ src/extension.ts
@@ -0,0 +1,10 @@
+import { window } from 'vscode';
+
+/**
+ * This method is called when the extension is activated. It initializes the core functionality of the extension.
+ */
+export function activate (): void {
+
+	window.showInformationMessage('Congratulations, your extension "Chimera Theme" is now active!');
+
+}
