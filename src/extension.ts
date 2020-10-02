b9ad1276cc2690cb35bcabbd93dedf5f97b22e7f
roydukkey <contact@changelog.me>
2020-10-02 19:29:08 -0400

    Listen for changes to configuration

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,17 @@
-import { window } from 'vscode';
+import { ExtensionContext, window, workspace } from 'vscode';
 
 /**
  * This method is called when the extension is activated. It initializes the core functionality of the extension.
+ *
+ * @param context - A collection of utilities private to an extension.
  */
-export function activate (): void {
+export function activate (context: ExtensionContext): void {
 
-	window.showInformationMessage('Congratulations, your extension "Chimera Theme" is now active!');
+	// When theme configuration changes...
+	context.subscriptions.push(workspace.onDidChangeConfiguration((event) => {
+		if (event.affectsConfiguration('theme-chimera.plus.contrastConstants')) {
+			window.showInformationMessage('Congratulations, the configuration for your extension has changed!');
+		}
+	}));
 
 }
