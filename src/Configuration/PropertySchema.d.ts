ad16ce0c93bec1dbd69de046ca1246da5a4e9ad6
roydukkey <contact@changelog.me>
2020-10-02 22:43:36 -0400

    Add types for configuration

---
+++ src/Configuration/PropertySchema.d.ts
@@ -0,0 +1,36 @@
+/**
+ * Schema for the extension's contributing {@link https://code.visualstudio.com/api/references/contribution-points#Configuration-property-schema | `configuration properties`}.
+ */
+interface PropertySchema<T> {
+
+	type?: string,
+	default: T,
+
+	enum?: T[],
+	enumDescriptions?: string[],
+
+	minimum?: T,
+	maximum?: T,
+
+	minLength?: number,
+	maxLength?: number,
+
+	minItems?: number,
+	maxItems?: number,
+
+	format?: string,
+
+	pattern?: RegExp,
+	patternErrorMessage?: string,
+
+	description?: string,
+	markdownDescription?: string,
+
+	deprecationMessage?: string,
+	markdownDeprecationMessage?: string,
+
+	scope?: string
+
+}
+
+export default PropertySchema;
---
+++ src/Configuration/index.d.ts
@@ -0,0 +1,2 @@
+export { default as PropertySchema } from './PropertySchema';
+export { default as WorkspaceConfiguration } from './Workspace';
